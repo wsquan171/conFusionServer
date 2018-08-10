4ddede6bd448c1cbe18ed207dd41da372f74e6f0
wsquan171 <wsquan171@outlook.com>
2018-08-09 21:20:30 -0700

    Express Session Part 1

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,11 +24,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('11ddcfd1-ffc5-4ae6-8b0a-81b57c28c182'));
+//app.use(cookieParser('11ddcfd1-ffc5-4ae6-8b0a-81b57c28c182'));
+app.use(session({
+    name: 'session-id',
+    secret: '11ddcfd1-ffc5-4ae6-8b0a-81b57c28c182',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 
 function auth (req, res, next) {
-    console.log(req.signedCookies);
-    if(!req.signedCookies.user)
+    console.log(req.session);
+    if(!req.session.user)
     {
         var authHeader = req.headers.authorization;
         if (!authHeader) {
@@ -41,7 +50,7 @@ function auth (req, res, next) {
         var user = auth[0];
         var pass = auth[1];
         if (user == 'admin' && pass == 'password') {
-            res.cookie('user', 'admin', {signed : true});
+            req.session.user = 'admin';
             next(); // authorized
         } 
         else {
@@ -54,14 +63,14 @@ function auth (req, res, next) {
     }
     else
     {
-        if(req.signedCookies.user == 'admin')
+        if (req.session.user === 'admin') {
+            console.log('req.session: ',req.session);
             next();
-        else
-        {
-            var err = new Error('You are not authenticated!');     
+        }
+        else {
+            var err = new Error('You are not authenticated!');
             err.status = 401;
             next(err);
-            return;
         }
     }
 }
