d18daa5b18f2b3d7e8f6733d4035372c5a5b4a60
wsquan171 <wsquan171@outlook.com>
2018-08-10 05:55:38 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     'secretKey': '111ddcfd1-ffc5-4ae6-8b0a-81b57c28c182',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '{your-app-id}',
+        clientSecret: '{your-app-secret}'
+    }
 }
 
